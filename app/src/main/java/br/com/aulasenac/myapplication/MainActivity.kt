fcc8a40b77418127cec01497945cd03142bf63dc
Gabr1elcastro <eg7112837@gmail.com>
2024-12-12 17:16:26 -0300

    Initial commit

---
+++ app/src/main/java/br/com/aulasenac/myapplication/MainActivity.kt
@@ -0,0 +1,29 @@
+package br.com.aulasenac.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import java.util.Random
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun sortear(view: View){
+        val textoResultado = findViewById<TextView>(R.id.text_resultado)
+        val numero = Random().nextInt(21)
+        textoResultado.setText("$numero")
+    }
+}
